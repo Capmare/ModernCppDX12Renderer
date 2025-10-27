8939b00b9b160fd6192f7b20cd527ed56ad07299
david <capmare.david@gmail.com>
2025-10-27 17:56:29 +0100

    Add new type of logging message and structure logging differently

--- src/Header/Logger/HOXLogger.h
+++ src/Header/Logger/HOXLogger.h
@@ -11,6 +11,7 @@ enum HOXSeverity {
     Normal,
     Warning,
     Error,
+    ErrorNoCrash,
     Info,
     Debug
 };
--- src/Source/Logger/HOXLogger.cpp
+++ src/Source/Logger/HOXLogger.cpp
@@ -8,25 +8,47 @@
 #include <iostream>
 #include <windows.h>
 
-void HOXLogger::Message(const HOXSeverity &Severity, const std::string &Message, const std::source_location& Location) {
-
+void HOXLogger::Message(const HOXSeverity &Severity, const std::string &Message, const std::source_location &Location) {
     std::string SeverityStr;
     std::string Color;
 
     switch (Severity) {
-        case HOXSeverity::Normal: SeverityStr = "NORMAL"; break;
-        case HOXSeverity::Error: SeverityStr = "ERROR"; Color = "\033[31m"; break;
-        case HOXSeverity::Warning: SeverityStr = "WARNING"; Color = "\033[33m"; break;
-        case HOXSeverity::Info: SeverityStr = "INFO"; Color = "\033[36m"; break;
-        case HOXSeverity::Debug: SeverityStr = "DEBUG"; Color = "\033[32m"; break;
-        default: SeverityStr = "UNKNOWN"; Color = "\033[0m"; break;
+        case HOXSeverity::Normal: SeverityStr = "NORMAL";
+            std::cout << Color << SeverityStr << "\033[0m"
+            << "\t" << Message << std::endl; return;
+            break;
+        case HOXSeverity::ErrorNoCrash:
+        case HOXSeverity::Error: SeverityStr = "ERROR";
+            Color = "\033[31m";
+            break;
+        case HOXSeverity::Warning: SeverityStr = "WARNING";
+            Color = "\033[33m";
+            break;
+        case HOXSeverity::Info: SeverityStr = "INFO";
+            Color = "\033[36m";
+            break;
+        case HOXSeverity::Debug: SeverityStr = "DEBUG";
+            Color = "\033[32m";
+            break;
+        default: SeverityStr = "UNKNOWN";
+            Color = "\033[0m";
+            break;
     }
 
     std::cout << Color << SeverityStr << "\033[0m"
-              << "\t" << Message
-              << "\t" << Location.file_name()
-              << ":" << Location.line()
-              << "\t" << Location.function_name()
-              << std::endl;
+            << "\t" << Message
+            << "\t" << Location.file_name()
+            << ":" << Location.line()
+            << "\t" << Location.function_name()
+            << std::endl;
 
+    if (Severity == HOXSeverity::Error) {
+        throw std::runtime_error(std::format(
+            "Message:\t{}\nLine:\t{}:{}\nIn function:\t{}",
+            Message,
+            Location.file_name(),
+            Location.line(),
+            Location.function_name()
+        ));
+    }
 }
