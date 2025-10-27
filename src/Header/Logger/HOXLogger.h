76c1dbf279fbb2cd96bc47684d8e52c08f050278
david <capmare.david@gmail.com>
2025-10-27 17:37:05 +0100

    Logger class created

---
+++ src/Header/Logger/HOXLogger.h
@@ -0,0 +1,36 @@
+﻿//
+// Created by david on 10/27/2025.
+//
+
+#ifndef MODERNCPPDX12RENDERER_HOXLOGGER_H
+#define MODERNCPPDX12RENDERER_HOXLOGGER_H
+#include <source_location>
+#include <string>
+
+enum HOXSeverity {
+    Normal,
+    Warning,
+    Error,
+    Info,
+    Debug
+};
+
+
+
+class HOXLogger {
+    public:
+    HOXLogger() = delete;
+    ~HOXLogger() = delete;
+
+    // Prevent copy and move
+    HOXLogger(const HOXLogger&) = delete;
+    HOXLogger& operator=(const HOXLogger&) = delete;
+    HOXLogger(HOXLogger&&) = delete;
+    HOXLogger& operator=(HOXLogger&&) = delete;
+
+    static void Message(const HOXSeverity& Severity, const std::string& Message, const std::source_location& Location = std::source_location::current());
+
+};
+
+
+#endif //MODERNCPPDX12RENDERER_HOXLOGGER_H
---
+++ src/Source/Logger/HOXLogger.cpp
@@ -0,0 +1,32 @@
+﻿//
+// Created by david on 10/27/2025.
+//
+
+#include "../../Header/Logger/HOXLogger.h"
+
+#include <format>
+#include <iostream>
+#include <windows.h>
+
+void HOXLogger::Message(const HOXSeverity &Severity, const std::string &Message, const std::source_location& Location) {
+
+    std::string SeverityStr;
+    std::string Color;
+
+    switch (Severity) {
+        case HOXSeverity::Normal: SeverityStr = "NORMAL"; break;
+        case HOXSeverity::Error: SeverityStr = "ERROR"; Color = "\033[31m"; break;
+        case HOXSeverity::Warning: SeverityStr = "WARNING"; Color = "\033[33m"; break;
+        case HOXSeverity::Info: SeverityStr = "INFO"; Color = "\033[36m"; break;
+        case HOXSeverity::Debug: SeverityStr = "DEBUG"; Color = "\033[32m"; break;
+        default: SeverityStr = "UNKNOWN"; Color = "\033[0m"; break;
+    }
+
+    std::cout << Color << SeverityStr << "\033[0m"
+              << "\t" << Message
+              << "\t" << Location.file_name()
+              << ":" << Location.line()
+              << "\t" << Location.function_name()
+              << std::endl;
+
+}
